c890b10a7340c6124f7d13c11a7985c32fdb5bd9
Christian Palacios <79600301+chris-palacios@users.noreply.github.com>
2026-07-20 18:31:49 -0700

    Started fresh React App

--- frontend/src/path.ts
+++
@@ -1,9 +0,0 @@
-function Path(args: String[]){
-    let path: string = "";
-    args.forEach(element => {
-        path = path + element
-    });
-    return path
-}
-
-export default Path
--- frontend/src/utils/image-url.ts
+++
@@ -1,5 +0,0 @@
-function getImageUrl(filename: string){
-    return new URL(`../assets/images/${filename}`, import.meta.url).href
-}
-
-export {getImageUrl};
